e528a1db22c2627cbebbec2b52c6d3ac52b24397
Emma L <emmanuel.loi@neptuness.com>
2021-05-16 19:59:24 +0300

    Added HW10 Task1. Implemented check value calculation

---
+++ src/com/homework10/Fruit.java
@@ -0,0 +1,69 @@
+package com.homework10;
+
+public class Fruit {
+
+    public enum Type {
+        APPLE, BANANA, LEMON
+    }
+
+    public enum Color {
+        GREEN, RED, YELLOW
+    }
+
+    private Type type;
+    private Color color;
+    private int weight;
+    private double pricePerKg;
+
+    public Fruit() {
+    }
+
+    public Fruit(Type type, Color color, int weight, double pricePerKg) {
+        this.type = type;
+        this.color = color;
+        this.weight = weight;
+        this.pricePerKg = pricePerKg;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public double getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setPricePerKg(double pricePerKg) {
+        this.pricePerKg = pricePerKg;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "type=" + type +
+                ", color=" + color +
+                ", weight=" + weight +
+                ", pricePerKg=" + pricePerKg +
+                '}';
+    }
+}
---
+++ src/com/homework10/Main.java
@@ -0,0 +1,24 @@
+package com.homework10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fruit greenApple = new Fruit(Fruit.Type.APPLE, Fruit.Color.GREEN, 346, 21.95);
+        Fruit redApple = new Fruit(Fruit.Type.APPLE, Fruit.Color.RED, 230, 19.95);
+        Fruit banana = new Fruit(Fruit.Type.BANANA, Fruit.Color.YELLOW, 220, 32.85);
+        Fruit lemon = new Fruit(Fruit.Type.LEMON, Fruit.Color.YELLOW, 88, 34.95);
+
+        double greenApplePrice = pricePerWeight(greenApple.getWeight(), greenApple.getPricePerKg());
+        double redApplePrice = pricePerWeight(redApple.getWeight(), redApple.getPricePerKg());
+        double bananaPrice = pricePerWeight(banana.getWeight(), banana.getPricePerKg());
+        double lemonPrice = pricePerWeight(lemon.getWeight(), lemon.getPricePerKg());
+
+        double checkAmount = greenApplePrice + redApplePrice + bananaPrice + lemonPrice;
+        System.out.printf("РЎСѓРјРјР° РїРѕ С‡РµРєСѓ: %.2f", checkAmount); // 7.59 РіСЂРЅ
+
+    }
+
+    public static double pricePerWeight(int fruitWeight, double pricePerKg) {
+        return pricePerKg * fruitWeight / 1000;
+    }
+}
